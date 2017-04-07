7869ae7681a810f4b6ff8879d9013a641b8526eb
David Shlemayev <secondlifedvi@gmail.com>
2017-04-07 23:11:12 +0300

    Program more useful, int. test runs the executable

--- integrationtests/exampletest.sh
+++ integrationtests/exampletest.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
+# Note this runs from the project root
+echo "Program execution:"
+./bin/mycompiler
 exit 0; # 0 means success, 1 means fail
--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include "testable.h"
+
 int main() {
+	printf("This should output the number six: %d\n", abc());
 	return 0;
 }
