4d7cc66bc48ce284890853111c3c2d8d00baef58
David Shlemayev <secondlifedvi@gmail.com>
2017-04-22 00:16:19 +0300

    Added utility functions, with one to get the last char of a string

---
+++ src/utilityfunctions.c
@@ -0,0 +1,5 @@
+char string_get_last_char (char* string) {
+	while (string[0] != '\0') string++;
+	string--;
+	return string[0];
+}
---
+++ src/utilityfunctions.h
@@ -0,0 +1,6 @@
+#ifndef UTILITYFUNCTIONS_H 
+#define UTILITYFUNCTIONS_H 
+
+char string_get_last_char (char* string);
+
+#endif
