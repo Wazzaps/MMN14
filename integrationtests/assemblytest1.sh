b7e5f8d4b2fa77f0ea9ee2d7bfbf2a47dc1b7a24
David <secondlifedvi@gmail.com>
2017-07-03 20:20:04 +0300

    Git wtf-ery

--- integrationtests/assemblytest1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Note this runs from the project root
-./bin/mycompiler assemblytest1 --tofolder temp > /dev/null
-exit 0; # 0 means success, 1 means fail
--- integrationtests/exampletest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Note this runs from the project root
-
-exit 0; # 0 means success, 1 means fail
